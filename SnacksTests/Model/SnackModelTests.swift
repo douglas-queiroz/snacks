3f71c54c28cc0b46dc29f11ca7ce05faf01f90aa
Douglas Queiroz <douglasq16@gmail.com>
2017-07-17 15:07:49 -0300

    Add snack and ingredients models and tests

--- SnacksTests/Model/SnackModelTests.swift
+++ SnacksTests/Model/SnackModelTests.swift
@@ -11,21 +11,6 @@ import XCTest
 
 class SnackModelTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
     func testCalculateSnackPrice() {
         let price1 = 2.5
         let price2 = 3.2
@@ -49,13 +34,6 @@ class SnackModelTests: XCTestCase {
         
         
         XCTAssertEqual(snack.price, (price1 + price2 + price3))
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
